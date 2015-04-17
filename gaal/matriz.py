c6ab502a347edabc9c73e98ed350a065a03c0cbc
Yudi Matsuzake <yudi.matsuzake@gmail.com>
2015-04-17 09:54:55 -0300

    DONE:
    	* Desistência de fazer em C pra aprender um pouco mais de python;
    	* Alterado pra python e estrutura feita **MELHORADO**
    	* linha de comando
    	* Ordem da matriz identidade deveria ser quadrada
    
    TODO:
    	* Matrizes e suas operações:
    		[ 1/2 ] Classe e estrutura
    		[ ] Operações
    			[ x ] Adição
    			[ x ] Subtração
    			[   ] Multiplicação
    				[ x ] com constante
    				[   ] de matrizes
    		[ x ] Impressão dela na tela de maneira maneira.
    		[ ] Funções para gerar matrizes
    			[ x ] Identidade
    			[ x ] Aleatória
    			[   ] Da entrada padrão
    				[ x ] Até o EOF
    				[   ] Com a ordem dada
    	* Arrumar a instalação

--- gaal/matriz.py
+++ gaal/matriz.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
 #----------Operações-----------
 #generico
 def operacao(A, B, op):
--- gaal/matrizes.py
+++ gaal/matrizes.py
@@ -3,12 +3,21 @@
 from matriz import *
 import random
 
-def identidade(n_linha=1, n_coluna=1):
-	"""Cria e retorna uma matriz identidade"""
-	M = Matriz(n_linha, n_coluna)
+#----------Auxiliares----------
+def num(x):
+	"""Converte x para um número. Tenta pra int, se não der, float"""
+	try:
+		return int(x)
+	except ValueError:
+		return float(x)
 
-	for i in range(M.n_linha):
-		for j in range(M.n_coluna):
+#------------------------------
+def identidade(ordem=1):
+	"""Cria e retorna uma matriz identidade de ordem n (default 1) """
+	M = Matriz(ordem, ordem)
+
+	for i in range(ordem):
+		for j in range(ordem):
 			if i == j:
 				M[i][j] = 1
 			else:
@@ -33,3 +42,50 @@ def aleatoria(n_linha=None, n_coluna=None, a=-100, b=100):
 			M[i][j] = random.randint(a, b)
 
 	return M
+
+
+#drom stdin
+
+def _build(L, n_coluna):
+	n_linha = len(L)
+	M = Matriz(n_linha, n_coluna, valor_padrao=None)
+	for i in range(M.n_linha):
+		for j in range(M.n_coluna):
+			M[i][j] = L[i][j]
+	return M
+
+def from_stdin():
+	"""
+	Lê a entrada padrão até o EOF e a transforma em uma matriz.
+	Amxn=
+	a00 .. a0n
+	..  ..
+	am0 .. amn
+	"""
+	n_largura=None
+	n_coluna=None
+	L = []
+	try:
+		#Lê da entrada padrão até dizer chega
+			#ctrl+d (EOF) ou entrada vazia
+		while True :
+			#Pega a entrada e transforma em números
+			l = ([ num(i) for i in str(raw_input()).split() ])
+
+			#Se nenhum valor foi dado pra coluna, dê o tamanho da lista
+			if n_coluna == None:
+				n_coluna = len(l)
+
+			#Se a entrada for nada
+			if len(l) <= 0:
+				return _build(L, n_coluna)
+
+			#O número de coluna deve ser igual para todas as linhas
+			if n_coluna != len(l):
+				return None
+
+			L.append(l)
+	except EOFError:
+		#EOF (ctrl+d)
+		return _build(L, n_coluna)
+		
